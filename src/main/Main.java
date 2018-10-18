89b5e41c5aa4a4e5a9a0dac624eb4e62f6c99118
marc-goodman <marc.goodman@pcc.edu>
2018-10-18 15:59:23 -0700

    Adding SwingUtilities.invokeLater to main method.

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,6 +10,11 @@ import java.util.ArrayList;
  */
 public class Main {
     public static void main(String[] args) {
-        Controller.start();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Controller.start();
+            }
+        });
     }
 }
